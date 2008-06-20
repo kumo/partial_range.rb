4bf0482fffb9b16095b1707dfe9da755604ff047
kumo <kumo@kumo.it>
2008-06-21 00:24:52 +0200

    some crazy unplanned optimisation

--- lib/partial_range.rb
+++ lib/partial_range.rb
@@ -4,6 +4,7 @@ class PartialRange
     @string = ""
     @array = []
     @ranges = []
+    @array_dirty = false
 
     options = options[0]
     if options.is_a? Array
@@ -22,10 +23,21 @@ class PartialRange
   end
 
   def to_s
+    if @array_dirty
+      convert_ranges
+      @array_dirty = false
+    elsif @range_dirty
+      convert_array
+      @range_dirty = false
+    end
     @string
   end
 
   def to_a
+    if @array_dirty
+      convert_ranges
+      @array_dirty = false
+    end
     @array
   end
 
@@ -34,11 +46,15 @@ class PartialRange
       @array << value.flatten
       parse_array(@array)
     else
-      if !parse_ranges(value)
-        @array << value
-        parse_array(@array)
+      puts "value is #{value} and ranges: #{@ranges.inspect} and array: #{@array.inspect} and string: #{@string.inspect}"
+      if !parse_ranges(value) # value cannot be added to an existing range
+        if !parse_array_values(value)
+          @range_dirty = true
+        end
       else
-        convert_ranges
+        @array_dirty = true
+        puts "ranges are dirty"
+        #convert_ranges
       end
     end
   end
@@ -48,6 +64,52 @@ class PartialRange
   end
 
   protected
+
+  def convert_array
+    lowest = highest = nil
+    result = []
+    @string = ""
+    @ranges = []
+
+    @array.each do |value|
+    if lowest == nil # populate the lowest value if necessary
+      lowest = value
+      elsif highest == nil # populate the highest value if necessary
+        if value == lowest.succ
+          highest = value
+        else
+          result << lowest
+          lowest = value
+        end
+      else # if we have both high and low values...
+        if value == highest.succ # does the value continue the range?
+          highest = value
+        else # new value doesn't continue range
+          result << "#{lowest}-#{highest}" # write the old range
+          highest = nil
+          lowest = value # use the value as the new lowest limit
+        end
+      end
+    end
+
+    if highest != nil # if there is a high value then finish the range
+      result << "#{lowest}-#{highest}"
+    elsif lowest != nil # if there is a low value then add the value
+      result << lowest
+    end
+
+    result.flatten.each do |r|
+      if ! r.is_a? Fixnum
+        low, high = r.split("-")
+
+        @ranges << Range.new(low.to_i, high.to_i)
+      #else
+      #  @ranges << Range.new(r, r)
+      end
+    end
+    
+    @string = result.flatten.join(",")
+  end
 
   def parse_array(array)
     lowest = highest = nil
@@ -150,6 +212,35 @@ class PartialRange
     return changed
   end
 
+  def parse_array_values(value)
+    if !@array.include? value
+      @array << value
+      @array.sort!
+      
+      pos = @array.index(value)
+      lower_value = @array[pos-1]
+      upper_value = @array[pos+1]
+      puts "lower value is #{lower_value} and upper value is #{upper_value}"
+      if lower_value == value - 1 and upper_value == value + 1
+        @ranges << Range.new(lower_value, upper_value)
+        @array.delete(value)
+        @array.delete(lower_value)
+        @array.delete(upper_value)
+        @array_dirty = true
+      elsif @array[pos-1] == value - 1
+        @ranges << Range.new(lower_value, value)
+        @array.delete(value)
+        @array.delete(lower_value)
+        @array_dirty = true
+      elsif @array[pos+1] == value + 1
+        @ranges << Range.new(value, upper_value)
+        @array.delete(value)
+        @array.delete(upper_value)
+        @array_dirty = true
+      end
+    end
+  end
+
   def ranges_include?(value)
     #puts "There are #{@ranges.size} ranges (#{@ranges.inspect}) to check for #{value} (#{value.class})"
 
@@ -164,7 +255,7 @@ class PartialRange
   end
 
   def convert_ranges
-    #puts "creating new array and string from ranges"
+    #puts "converting an existing range"
     @array = []
     @string = []
     @ranges.each do |range|
@@ -175,5 +266,10 @@ class PartialRange
 
     @array.flatten!
     @string = @string.join(",")
+  end
+
+  def rebuild_array
+    puts "rebuilding array"
+    parse_array(@array)
   end
 end
