d7c6d5533537e592b61037f831b15573e886c156
kumo <kumo@kumo.it>
2008-06-20 22:38:18 +0200

    profiling the same thing multiple times

--- test/profile.rb
+++ test/profile.rb
@@ -2,11 +2,75 @@ require File.join(File.dirname(__FILE__), *%w[.. lib partial_range])
 require 'benchmark'
 
 Benchmark.bmbm do |x|
-  x.report("1000 times sequential"){
+  x.report("10 sequential numbers"){
+    pm = PartialRange.new
+
+    10.times do |i|
+      pm << i
+    end
+  }
+
+  x.report("100 sequential numbers"){
+    pm = PartialRange.new
+
+    100.times do |i|
+      pm << i
+    end
+  }
+
+  x.report("200 sequential numbers"){
+    pm = PartialRange.new
+
+    200.times do |i|
+      pm << i
+    end
+  }
+
+  x.report("1000 sequential numbers"){
     pm = PartialRange.new
 
     1000.times do |i|
       pm << i
+    end
+  }
+
+  x.report("2000 sequential numbers"){
+    pm = PartialRange.new
+
+    2000.times do |i|
+      pm << i
+    end
+  }
+
+  x.report("5000 sequential numbers"){
+    pm = PartialRange.new
+
+    5000.times do |i|
+      pm << i
+    end
+  }
+
+  x.report("10 times same"){
+    pm = PartialRange.new
+
+    10.times do |i|
+      pm << 1
+    end
+  }
+
+  x.report("100 times same"){
+    pm = PartialRange.new
+
+    100.times do |i|
+      pm << 1
+    end
+  }
+
+  x.report("200 times same"){
+    pm = PartialRange.new
+
+    200.times do |i|
+      pm << 1
     end
   }
 
@@ -18,11 +82,91 @@ Benchmark.bmbm do |x|
     end
   }
 
+  x.report("2000 times same"){
+    pm = PartialRange.new
+
+    2000.times do |i|
+      pm << 1
+    end
+  }
+
+  x.report("5000 times same"){
+    pm = PartialRange.new
+
+    5000.times do |i|
+      pm << 1
+    end
+  }
+
+  x.report("10 times *2"){
+    pm = PartialRange.new
+
+    10.times do |i|
+      pm << i * 2
+    end
+  }
+
+  x.report("100 times *2"){
+    pm = PartialRange.new
+
+    100.times do |i|
+      pm << i * 2
+    end
+  }
+
+  x.report("200 times *2"){
+    pm = PartialRange.new
+
+    200.times do |i|
+      pm << i * 2
+    end
+  }
+
   x.report("1000 times *2"){
     pm = PartialRange.new
 
     1000.times do |i|
-      pm << i*2
+      pm << i * 2
+    end
+  }
+
+  x.report("2000 times *2"){
+    pm = PartialRange.new
+
+    2000.times do |i|
+      pm << i * 2
+    end
+  }
+
+  x.report("5000 times *2"){
+    pm = PartialRange.new
+
+    5000.times do |i|
+      pm << i * 2
+    end
+  }
+
+  x.report("10 times random"){
+    pm = PartialRange.new
+
+    10.times do |i|
+      pm << rand(200000) # big number otherwise too similar to 'i'
+    end
+  }
+
+  x.report("100 times random"){
+    pm = PartialRange.new
+
+    100.times do |i|
+      pm << rand(200000) # big number otherwise too similar to 'i'
+    end
+  }
+
+  x.report("200 times random"){
+    pm = PartialRange.new
+
+    200.times do |i|
+      pm << rand(200000) # big number otherwise too similar to 'i'
     end
   }
 
@@ -30,7 +174,23 @@ Benchmark.bmbm do |x|
     pm = PartialRange.new
 
     1000.times do |i|
-      pm << rand(100000) # big number otherwise too similar to 'i'
+      pm << rand(200000) # big number otherwise too similar to 'i'
+    end
+  }
+
+  x.report("2000 times random"){
+    pm = PartialRange.new
+
+    2000.times do |i|
+      pm << rand(200000) # big number otherwise too similar to 'i'
+    end
+  }
+
+  x.report("5000 times random"){
+    pm = PartialRange.new
+
+    5000.times do |i|
+      pm << rand(200000) # big number otherwise too similar to 'i'
     end
   }
 end
